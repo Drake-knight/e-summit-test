fff39186673df89e862a14d7038ab9e617f4d96b
vishal <jakharvishalv@gmail.com>
2023-12-17 00:24:29 +0530

    asd

--- components/CustomPromptPWA/ManualPrompt.jsx
+++ components/CustomPromptPWA/ManualPrompt.jsx
@@ -1,72 +1,74 @@
-import React from "react";
+import React, { Fragment } from "react";
+import ShareIcon from "./ShareIcon";
+import HomeScreenIcon from "./HomeScreenIcon";
+import styles from "./PWAPrompt.styles.module.scss";
+import { confirmAlert } from "react-confirm-alert";
 
-import PWAPrompt from "./PWAprompt";
-
-const deviceCheck = () => {
-  const isiOS = /iphone|ipad|ipod/.test(
-    window.navigator.userAgent.toLowerCase()
-  );
-  const isiPadOS =
-    navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1;
-  const isStandalone =
-    "standalone" in window.navigator && window.navigator.standalone;
-
-  return (isiOS || isiPadOS) && !isStandalone;
+export const manualPrompt = async ({ doNotShowAgain, alreadyInstalled, isIOS, isAndroid }) => {
+  confirmAlert({
+    customUI: ({ onClose: close }) => (
+      <Fragment>
+        <div
+          className={`${styles.pwaPromptOverlay} iOSPWA-overlay`}
+          aria-label="Close overlay"
+          role="button"
+          onClick={close}
+        />
+        <div
+          className={`${styles.pwaPrompt} iOSPWA-container`}
+          aria-describedby="pwa-prompt-description"
+          aria-labelledby="pwa-prompt-title"
+          role="dialog"
+        >
+          <div className={`${styles.pwaPromptHeader} iOSPWA-header`}>
+            <p
+              id="pwa-prompt-title"
+              className={`${styles.pwaPromptTitle} iOSPWA-title`}
+            >
+              Add to Home Screen
+            </p>
+            <button
+              className={`${styles.pwaPromptCancel} iOSPWA-cancel`}
+              onClick={close}
+              aria-label="Cancel"
+            >
+              Cancel
+            </button>
+          </div>
+          <div className={`${styles.pwaPromptBody} iOSPWA-body`}>
+            <div className={`${styles.pwaPromptDescription} iOSPWA-description`}>
+              <p
+                id="pwa-prompt-description"
+                className={`${styles.pwaPromptCopy} iOSPWA-description-copy`}
+              >
+                This website has app functionality. Add it to your home screen to use it in fullscreen and while offline.
+              </p>
+            </div>
+          </div>
+          <div className={`${styles.pwaPromptInstruction} iOSPWA-steps`}>
+            <div className={`${styles.pwaPromptInstructionStep} iOSPWA-step1`}>
+              <ShareIcon
+                className={`${styles.pwaPromptShareIcon} iOSPWA-step1-icon`}
+              />
+              <p
+                className={`${styles.pwaPromptCopy} ${styles.bold} iOSPWA-step1-copy`}
+              >
+                1. Press the 'Share' button on the menu bar below.
+              </p>
+            </div>
+            <div className={`${styles.pwaPromptInstructionStep} iOSPWA-step2`}>
+              <HomeScreenIcon
+                className={`${styles.pwaPromptHomeIcon} iOSPWA-step2-icon`}
+              />
+              <p
+                className={`${styles.pwaPromptCopy} ${styles.bold} iOSPWA-step2-copy`}
+              >
+                2. Press 'Add to Home Screen'.
+              </p>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    ),
+  });
 };
-
-const PromptWrapper = ({
-  timesToShow = 1,
-  promptOnVisit = 1,
-  permanentlyHideOnDismiss = true,
-  copyTitle = "Add to Home Screen",
-  copyBody = "This website has app functionality. Add it to your home screen to use it in fullscreen and while offline.",
-  copyShareButtonLabel = "1) Press the 'Share' button on the menu bar below.",
-  copyAddHomeButtonLabel = "2) Press 'Add to Home Screen'.",
-  copyClosePrompt = "Cancel",
-  delay = 1000,
-  debug = false,
-  onClose = () => {},
-}) => {
-  let promptData = JSON.parse(localStorage.getItem("iosPwaPrompt"));
-
-  if (promptData === null) {
-    promptData = { isiOS: deviceCheck(), visits: 0 };
-    localStorage.setItem("iosPwaPrompt", JSON.stringify(promptData));
-  }
-
-  if (promptData.isiOS || debug) {
-    const aboveMinVisits = promptData.visits + 1 >= promptOnVisit;
-    const belowMaxVisits = promptData.visits + 1 < promptOnVisit + timesToShow;
-
-    if (belowMaxVisits || debug) {
-      localStorage.setItem(
-        "iosPwaPrompt",
-        JSON.stringify({
-          ...promptData,
-          visits: promptData.visits + 1,
-        })
-      );
-
-      if (aboveMinVisits || debug) {
-        return (
-          <PWAPrompt
-            delay={delay}
-            copyTitle={copyTitle}
-            copyBody={copyBody}
-            copyAddHomeButtonLabel={copyAddHomeButtonLabel}
-            copyShareButtonLabel={copyShareButtonLabel}
-            copyClosePrompt={copyClosePrompt}
-            permanentlyHideOnDismiss={permanentlyHideOnDismiss}
-            promptData={promptData}
-            maxVisits={timesToShow + promptOnVisit}
-            onClose={onClose}
-          />
-        );
-      }
-    }
-  }
-
-  return null;
-};
-
-export default PromptWrapper;
