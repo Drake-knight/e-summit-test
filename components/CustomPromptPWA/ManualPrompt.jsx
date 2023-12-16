20cf317d01b762048c8ff9ec3063441d4a58b09b
vishal <jakharvishalv@gmail.com>
2023-12-16 23:18:37 +0530

    ios-prompt

--- components/CustomPromptPWA/ManualPrompt.jsx
+++ components/CustomPromptPWA/ManualPrompt.jsx
@@ -1,92 +1,72 @@
-import { Icon } from "@iconify/react"
-import { Button, Card } from "flowbite-react"
-import { confirmAlert } from "react-confirm-alert"
+import React from "react";
 
-// This Prompt is only for devices which have PWA support and cannot install automatically
-export const manualPrompt = async ({ doNotShowAgain, alreadyInstalled, isIOS, isAndroid } ) => {
-    confirmAlert({
-        customUI: ({ onClose: close }) => {
-            return <Card horizontal={true} className="bg-white" style={{ width: "auto" }}>
-                <div className="relative -top-4 -right-4 text-[#F62B2B]">
-                    <div className="flex justify-end">
-                        <Icon
-                            icon="zondicons:close-solid"
-                            width={20}
-                            className="mr-3 h-6 sm:h-9 cursor-pointer"
-                            onClick={close}
-                        />
-                    </div>
-                </div>
-                <div className="flex flex-col gap-6 -mt-12">
-                    <div className="flex flex-row justify-between gap-6">
-                        <div className="flex items-start -mr-6">
-                            <Icon
-                                icon="twemoji:books"
-                                width={42}
-                                className="mr-3 h-6 sm:h-9"
-                            />
-                        </div>
-                        <div className="flex flex-col gap-2">
-                            <h4 className="text-[18px] font-bold">
-                                Install My Books app?
-                            </h4>
-                            <p className="text-[14px]">
-                                We would recommend installing the app for better experience.
-                            </p>
-                            <div className="flex justify-start">
-                                {/* Prompt for ios Devices */}
-                                {isIOS && !isAndroid ?
-                                    <div className="flex flex-col gap-2 mt-2 text-[14px]">
-                                        <div className="inline-flex items-center gap-2 font-bold">
-                                            1.  Click the <Icon icon="material-symbols:ios-share" /> icon
-                                        </div>
-                                        2. Scroll down and then click
-                                        <div className="inline-flex items-center gap-2 font-bold">
-                                            3.  Add to Home Screen <Icon icon="icon-park-outline:add" />
-                                        </div>
-                                    </div>
-                                    :
-                                    <div className="flex flex-col gap-2 mt-2 text-[14px]">
-                                        <div className="inline-flex items-center gap-2 font-bold">
-                                            1. Click the <Icon icon="zondicons:dots-horizontal-triple" /> icon
-                                        </div>
-                                        2.  Scroll down and then click
-                                        <div className="inline-flex items-center gap-2 font-bold">
-                                            3. <Icon icon="ic:baseline-add-to-home-screen" /> Add to Home Screen
-                                        </div>
-                                    </div>
-                                }
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col sm:flex-row gap-2">
-                        <Button
-                            color="gray"
-                            className="w-full"
-                            onClick={() => alreadyInstalled(close)}
-                        >
-                            {"I've already installed"}
-                            <Icon
-                                icon="fluent:checkbox-checked-20-filled"
-                                className="ml-2 text-green-500"
-                                width={20}
-                            />
-                        </Button>
-                        <Button
-                            color="gray"
-                            className="w-full"
-                            onClick={() => doNotShowAgain(close)}
-                        >
-                            {"Don't Show it again"}
-                            <Icon
-                                icon="teenyicons:message-no-access-solid"
-                                className="ml-2 text-blue-500"
-                                width={20}
-                            />
-                        </Button>
-                    </div>
-                </div>
-            </Card>
-        },
-    })
-}
+import PWAPrompt from "./PWAprompt";
+
+const deviceCheck = () => {
+  const isiOS = /iphone|ipad|ipod/.test(
+    window.navigator.userAgent.toLowerCase()
+  );
+  const isiPadOS =
+    navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1;
+  const isStandalone =
+    "standalone" in window.navigator && window.navigator.standalone;
+
+  return (isiOS || isiPadOS) && !isStandalone;
+};
+
+const PromptWrapper = ({
+  timesToShow = 1,
+  promptOnVisit = 1,
+  permanentlyHideOnDismiss = true,
+  copyTitle = "Add to Home Screen",
+  copyBody = "This website has app functionality. Add it to your home screen to use it in fullscreen and while offline.",
+  copyShareButtonLabel = "1) Press the 'Share' button on the menu bar below.",
+  copyAddHomeButtonLabel = "2) Press 'Add to Home Screen'.",
+  copyClosePrompt = "Cancel",
+  delay = 1000,
+  debug = false,
+  onClose = () => {},
+}) => {
+  let promptData = JSON.parse(localStorage.getItem("iosPwaPrompt"));
+
+  if (promptData === null) {
+    promptData = { isiOS: deviceCheck(), visits: 0 };
+    localStorage.setItem("iosPwaPrompt", JSON.stringify(promptData));
+  }
+
+  if (promptData.isiOS || debug) {
+    const aboveMinVisits = promptData.visits + 1 >= promptOnVisit;
+    const belowMaxVisits = promptData.visits + 1 < promptOnVisit + timesToShow;
+
+    if (belowMaxVisits || debug) {
+      localStorage.setItem(
+        "iosPwaPrompt",
+        JSON.stringify({
+          ...promptData,
+          visits: promptData.visits + 1,
+        })
+      );
+
+      if (aboveMinVisits || debug) {
+        return (
+          <PWAPrompt
+            delay={delay}
+            copyTitle={copyTitle}
+            copyBody={copyBody}
+            copyAddHomeButtonLabel={copyAddHomeButtonLabel}
+            copyShareButtonLabel={copyShareButtonLabel}
+            copyClosePrompt={copyClosePrompt}
+            permanentlyHideOnDismiss={permanentlyHideOnDismiss}
+            promptData={promptData}
+            maxVisits={timesToShow + promptOnVisit}
+            onClose={onClose}
+          />
+        );
+      }
+    }
+  }
+
+  return null;
+};
+
+export default PromptWrapper;
