3b1f87bb9709d363fc97ee35a34924ee38902ccf
vishal <jakharvishalv@gmail.com>
2023-12-17 13:16:03 +0530

    ios-prompt

--- components/CustomPromptPWA/ManualPrompt.jsx
+++ components/CustomPromptPWA/ManualPrompt.jsx
@@ -1,74 +1,71 @@
 import React, { Fragment } from "react";
 import ShareIcon from "./ShareIcon";
 import HomeScreenIcon from "./HomeScreenIcon";
-import styles from "./PWAPrompt.styles.module.scss";
+import styles from "./PWAPrompt.module.css";
 import { confirmAlert } from "react-confirm-alert";
 
 export const manualPrompt = async () => {
   confirmAlert({
-    customUI: ({ onClose: close }) => (
+    customUI: ({ onClose }) => (
       <Fragment>
-        <div
-          className={`${styles.pwaPromptOverlay} iOSPWA-overlay`}
-          aria-label="Close overlay"
+      <div
+          className={`${styles.pwaPromptOverlay}  iOSPWA-overlay`}
+          aria-label="Close"
           role="button"
-          onClick={close}
-        />
-        <div
-          className={`${styles.pwaPrompt} iOSPWA-container`}
+          onClick={onClose}
+      />
+      <div
+          className={`${styles.pwaPrompt}  iOSPWA-container`}
           aria-describedby="pwa-prompt-description"
           aria-labelledby="pwa-prompt-title"
           role="dialog"
-        >
+      >
           <div className={`${styles.pwaPromptHeader} iOSPWA-header`}>
-            <p
-              id="pwa-prompt-title"
-              className={`${styles.pwaPromptTitle} iOSPWA-title`}
-            >
+              <p id="pwa-prompt-title" className={`${styles.pwaPromptTitle} iOSPWA-title`}>
               Add to Home Screen
-            </p>
-            <button
-              className={`${styles.pwaPromptCancel} iOSPWA-cancel`}
-              onClick={close}
-              aria-label="Cancel"
-            >
-              Cancel
-            </button>
+              </p>
+              <button
+                  className={`${styles.pwaPromptCancel} iOSPWA-cancel`}
+                  onClick={onClose}
+              >
+                  Cancel
+              </button>
           </div>
           <div className={`${styles.pwaPromptBody} iOSPWA-body`}>
-            <div className={`${styles.pwaPromptDescription} iOSPWA-description`}>
-              <p
-                id="pwa-prompt-description"
-                className={`${styles.pwaPromptCopy} iOSPWA-description-copy`}
-              >
-                This website has app functionality. Add it to your home screen to use it in fullscreen and while offline.
-              </p>
-            </div>
+              <div className={`${styles.pwaPromptDescription} iOSPWA-description`}>
+                  <p
+                      id="pwa-prompt-description"
+                      className={`${styles.pwaPromptCopy} iOSPWA-description-copy`}
+                  >
+                      This website has app functionality. Add it to your home screen to use it in fullscreen and while offline.
+                  </p>
+              </div>
           </div>
           <div className={`${styles.pwaPromptInstruction} iOSPWA-steps`}>
-            <div className={`${styles.pwaPromptInstructionStep} iOSPWA-step1`}>
-              <ShareIcon
-                className={`${styles.pwaPromptShareIcon} iOSPWA-step1-icon`}
-              />
-              <p
-                className={`${styles.pwaPromptCopy} ${styles.bold} iOSPWA-step1-copy`}
-              >
-                1. Press the &apos;Share&apos; button on the menu bar below.
-              </p>
-            </div>
-            <div className={`${styles.pwaPromptInstructionStep} iOSPWA-step2`}>
-              <HomeScreenIcon
-                className={`${styles.pwaPromptHomeIcon} iOSPWA-step2-icon`}
-              />
-              <p
-                className={`${styles.pwaPromptCopy} ${styles.bold} iOSPWA-step2-copy`}
-              >
-                2. Press &apos;Add to Home Screen&apos;.
-              </p>
-            </div>
+              <div className={`${styles.pwaPromptInstructionStep} iOSPWA-step1`}>
+                  <ShareIcon
+                      className={`${styles.pwaPromptShareIcon} iOSPWA-step1-icon`}
+                  />
+                  <p
+                      className={`${styles.pwaPromptCopy} ${styles.bold} iOSPWA-step1-copy`}
+                  >
+                      Press the &apos;Share&apos; button on the menu bar below.
+                  </p>
+              </div>
+              <div className={`${styles.pwaPromptInstructionStep} iOSPWA-step2`}>
+                  <HomeScreenIcon
+                      className={`${styles.pwaPromptHomeIcon} iOSPWA-step2-icon`}
+                      
+                  />
+                  <p
+                      className={`${styles.pwaPromptCopy} ${styles.bold} iOSPWA-step2-copy`}
+                  >
+                      Press &apos;Add to Home Screen&apos;.
+                  </p>
+              </div>
           </div>
-        </div>
-      </Fragment>
+      </div>
+  </Fragment>
     ),
   });
 };
