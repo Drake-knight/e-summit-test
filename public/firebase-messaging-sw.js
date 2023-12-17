a39f7a7ed7362d37515dec93be3bb17bb8819d20
vishal <jakharvishalv@gmail.com>
2023-12-18 02:14:48 +0530

    asda

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,10 +3,16 @@ importScripts("https://www.gstatic.com/firebasejs/8.3.2/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.3.2/firebase-messaging.js");
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCHTCHrkx75W46v7iJ4cPTEqhC4ygMeCs0",
-    authDomain: "esummit-86c5c.firebaseapp.com",
-    projectId: "esummit-86c5c",
-    storageBucket: "esummit-86c5c.appspot.com",
-    messagingSenderId: "927886998692",
-    appId: "1:927886998692:web:e04743740d20e82e81fa31",
+    apiKey: "AIzaSyBrYtnV3LkpRwa6SL8XW643OSdeIyAVjFY",
+
+    authDomain: "eeee-3074a.firebaseapp.com",
+
+    projectId: "eeee-3074a",
+
+    storageBucket: "eeee-3074a.appspot.com",
+
+    messagingSenderId: "370317250525",
+
+    appId: "1:370317250525:web:521af4f23954b5441e7034"
+
 });
