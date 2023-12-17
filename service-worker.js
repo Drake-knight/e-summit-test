e574f969fe2534aa62fb2667e4a96191700f94df
vishal <jakharvishalv@gmail.com>
2023-12-18 00:54:49 +0530

    d

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+import { clientsClaim } from "workbox-core";
+
+self.skipWaiting();
+clientsClaim();
+cleanupOutdatedCaches();
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener("fetch", (event) => {
+    if (!navigator.onLine) {
+        event.respondWith(
+            new Response(JSON.stringify({ type: "offline" }), {
+                headers: { "Content-Type": "application/json" },
+            })
+        );
+    }
+});
+
+self.addEventListener("push", (event) => {
+    const eventData = event.data.json();
+
+    const options = {
+        body: eventData.body,
+        icon: "/notification-icon.png",
+    };
+
+    event.waitUntil(self.registration.showNotification(eventData.title, options));
+});
+
+// self.addEventListener('notificationclick', function (event) {
+//     event.notification.close()
+//     event.waitUntil(
+//         clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
+//             if (clientList.length > 0) {
+//                 let client = clientList[0]
+//                 for (let i = 0; i < clientList.length; i++) {
+//                     if (clientList[i].focused) {
+//                         client = clientList[i]
+//                     }
+//                 }
+//                 return client.focus()
+//             }
+//             return clients.openWindow('/')
+//         })
+//     )
+// })
