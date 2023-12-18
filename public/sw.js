21f01b8d4ab1dc0c03911ed3e60ed7e7f1248e38
vishal <jakharvishalv@gmail.com>
2023-12-18 17:10:34 +0530

    push-notification-final

--- public/sw.js
+++ public/sw.js
@@ -1,21 +1,74 @@
+/* eslint-disable no-restricted-globals */
+const CACHE_NAME = "build0";
+const urlsToCache = ["index.html", "offline.html"];
 
-import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
-import { clientsClaim } from "workbox-core";
-
-self.skipWaiting();
-clientsClaim();
-cleanupOutdatedCaches();
-
-precacheAndRoute(self.__WB_MANIFEST);
+self.addEventListener("install", (event) => {
+    console.log("install event received:", event);
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log("Opened cache");
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
 
 self.addEventListener("fetch", (event) => {
-    if (!navigator.onLine) {
-        event.respondWith(
-            new Response(JSON.stringify({ type: "offline" }), {
-                headers: { "Content-Type": "application/json" },
-            })
-        );
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request).catch(() => caches.match("offline.html"));
+        })
+    );
+});
+
+self.addEventListener("push", (event) => {
+    const options = {
+        body: event.data.text(),
+        icon: "<public/logo192.png",
+    };
+
+    event.waitUntil(
+        self.registration.showNotification("Push Notification Title", options)
+    );
+});
+
+self.addEventListener("message", (event) => {
+    console.log(event);
+    if (event.data.type === "pushNotification") {
+        const data = event.data.data;
+
+        const pushOptions = {
+            body: data,
+            icon: "<public/logo192.png",
+        };
+
+        self.registration.showNotification("Push Notification Title", pushOptions);
+    } else if (event.data.type === "nameChangeNotification") {
+        const newName = event.data.data;
+        const message = `Hero Name Changed: ${newName}`;
+
+        const options = {
+            body: message,
+            icon: "/path/to/icon.png",
+        };
+
+        self.registration.showNotification("Name Changed", options);
     }
+});
+
+self.addEventListener("activate", (event) => {
+    const cacheWhiteList = [CACHE_NAME];
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhiteList.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener("push", (event) => {
