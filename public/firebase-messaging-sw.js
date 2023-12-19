4e050b9abd1a0d364d7288825df71b7c1d86f5a6
vishal <jakharvishalv@gmail.com>
2023-12-19 17:04:53 +0530

    dasd

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,12 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+
+console.info("Firebase messaging service worker is set up");
+
+// eslint-disable-next-line no-undef
+importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js');
+// eslint-disable-next-line no-undef
+importScripts('https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js');
+
 const firebaseConfig = {
     apiKey: "AIzaSyCHTCHrkx75W46v7iJ4cPTEqhC4ygMeCs0",
     authDomain: "esummit-86c5c.firebaseapp.com",
@@ -11,8 +16,20 @@ const firebaseConfig = {
     appId: "1:927886998692:web:e04743740d20e82e81fa31",
     measurementId: "G-YMKZWZF0RM",
 };
-
+// eslint-disable-next-line no-undef
 firebase.initializeApp(firebaseConfig);
+// eslint-disable-next-line no-undef
 const messaging = firebase.messaging();
 
-console.info("Firebase messaging service worker is set up");
+messaging.onBackgroundMessage((payload) => {
+    console.log(
+        '[firebase-messaging-sw.js] Received background message ',
+        payload
+    );
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: './logo.png',
+    };
+    self.registration.showNotification(notificationTitle, notificationOptions);
+});
