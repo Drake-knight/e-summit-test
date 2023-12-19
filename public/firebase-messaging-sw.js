ae5cd321433a27a8aa950e82412d3d5609cc15a3
vishal <jakharvishalv@gmail.com>
2023-12-19 16:22:09 +0530

    fdf

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,2 +1,19 @@
+/* eslint-disable no-undef */
+importScripts('https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.6.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHTCHrkx75W46v7iJ4cPTEqhC4ygMeCs0",
+    authDomain: "esummit-86c5c.firebaseapp.com",
+    projectId: "esummit-86c5c",
+    storageBucket: "esummit-86c5c.appspot.com",
+    messagingSenderId: "927886998692",
+    appId: "1:927886998692:web:e04743740d20e82e81fa31",
+    measurementId: "G-YMKZWZF0RM",
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
 console.info("Firebase messaging service worker is set up");
 console.log(self.__WB_MANIFEST);
